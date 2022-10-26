9d98fefa3bb4fb93279d5f7764c389f734d068ca
Konstantin Dobrychev <dobrychev.konstantin@gmail.com>
2022-10-26 17:49:40 +0300

    changed default device

--- 02/main.py
+++ 02/main.py
@@ -3,7 +3,7 @@ from devices.Device import *
 
 if __name__ == '__main__':
     try:
-        device: Device = open_device('/devices/dev1')
+        device: Device = open_device('/devices/dev5')
 
         for _ in range(3):
             print(read_line(device))
